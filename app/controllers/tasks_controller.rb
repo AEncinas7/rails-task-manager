6f0c9d670f79e7eb27ad89a668f265b15bc48620
Alexander Encinas <alexcromwell777@gmail.com>
2020-08-11 16:09:34 -0400

    Rails Task Manager challenge completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+ 
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    #  Parameters: {"task"=>{"title"=>"Exercise", "details"=>"For 1 hour"}}
+    @task = Task.create(create_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task = @task.update(create_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def create_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
